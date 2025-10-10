64195635a49f532d540219247969e0216d3e27e1
MAMAOUTALIBE <103372164+MAMAOUTALIBE@users.noreply.github.com>
2025-10-10 22:22:37 +0200

    Make email sending non-blocking

--- tickets_bah/utils.py
+++ tickets_bah/utils.py
@@ -1,5 +1,9 @@
+import logging
 from django.core.mail import send_mail
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
 
 def envoyer_confirmation_reservation(utilisateur, reservation):
     sujet = "Confirmation de votre réservation"
@@ -20,4 +24,8 @@ def envoyer_confirmation_reservation(utilisateur, reservation):
     destinataire = [utilisateur.email]
 
     # Envoi de l'e-mail
-    send_mail(sujet, message, settings.DEFAULT_FROM_EMAIL, destinataire)
+    try:
+        send_mail(sujet, message, settings.DEFAULT_FROM_EMAIL, destinataire)
+    except Exception as exc:
+        # Ne pas bloquer le flux si l'envoi d'email échoue (ex: SMTP mal configuré)
+        logger.warning("Échec de l'envoi de l'email de confirmation : %s", exc)
